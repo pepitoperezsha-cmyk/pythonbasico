9f50b18b868a107ee800bd2dfe0a1711ad4d70b0
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-02-13 15:27:09 -0800

    nose sesion_03.py

--- sesion_02.py
+++ sesion_02.py
@@ -3,18 +3,18 @@ mi_tupla= (2,4)
 print ("mi tupla: ", mi_tupla)
 
 #lista 
-mi_lista = [1, 3.1416, "Pablo ", mi_tupla]
+mi_lista = [1, 3.1416, "angel", mi_tupla]
 print("El peimwe elemento de mi lista ", mi_lista[0])
 print("El cuarto elemento de mi lista ", mi_lista[3])
 print("El tercer elemento de mi lista ", mi_lista[2])
 
 #diccionario 
-mi_dic = {
+mi_diccionario = {
     "mi_lista": mi_lista,
     "nombre": "angel",
     "Pi": 3.1416,
     "tel": "7485858344"
 }
-print("llave para accesar a mi diccionario mi_lista", mi_dic["mi_lista"])
-print("llave para accesar a mi diccionario Pi", mi_dic["Pi"])
-print("llave para accesar a mi diccionario tel", mi_dic["tel"])
+print("llave para accesar a mi diccionario mi_lista", mi_diccionario["mi_lista"])
+print("llave para accesar a mi diccionario Pi", mi_diccionario["Pi"])
+print("llave para accesar a mi diccionario tel", mi_diccionario["tel"])
--- sesion_03.py
+++ sesion_03.py
@@ -16,3 +16,14 @@ mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 for i in mi_lista_2:
        if i != "Lunes":
               print(f"Feliz{i}!")
+
+# while loop
+i = 0
+
+while i < 5 :
+       i += 1
+       if i==3:
+        continue
+       print(i)
+else:
+      print("i es ahora mayor o igual a 5")
