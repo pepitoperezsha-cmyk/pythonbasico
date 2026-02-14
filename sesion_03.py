df0e76b57e6045074df87c584a5d27ae73e24201
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-02-13 16:05:54 -0800

    nose sesion_03.py

--- sesion_03.py
+++ sesion_03.py
@@ -21,9 +21,37 @@ for i in mi_lista_2:
 i = 0
 
 while i < 5 :
-       i += 1
-       if i==3:
-        continue
-       print(i)
-else:
-      print("i es ahora mayor o igual a 5")
+    i += 1
+    if i ==3:
+          continue
+    print(i)
+    if i == 4: 
+          break
+    else:
+          print("i es ahora mayor o igual a 5")
+
+
+#practica 2
+#Dada la lista mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+#Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluya 
+#pista. usa los dos tipos loops while y for en el mismo programa para lograrlo
+#resultado 
+#martes
+#miercoles 
+#jueves 
+#viernes 
+#martes
+#miercoles
+#jueves
+#viernes
+#martes
+#miercoles
+#jueves
+#viernes
+i = 0
+mi_lista_2 = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+while i < 3 :
+    i += 1
+    for d in mi_lista_2:
+          if d !="lunes":
+                print(f"{d}!")
